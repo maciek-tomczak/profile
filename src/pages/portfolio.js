b126ba31fd6d35df69fc03b394d004103ee8f4da
maciek-tomczak <maciektomr@gmail.com>
2021-05-23 15:17:16 +0100

    fix brackets

--- src/pages/portfolio.js
+++ src/pages/portfolio.js
@@ -12,11 +12,11 @@ const SecondPage = () => (
             <li><i>2020</i> &nbsp;&nbsp;<Link to="https://youtu.be/HqDkCFoAn8M">DAOKO × Kenshi Yonezu - Uchiage Hanabi</Link></li>
             <li><i>2020</i> &nbsp;&nbsp;<Link to="https://youtu.be/A6NMQ9UKlqI">Aldnoah.Zero ending (ED 2) - aLIEz</Link></li>
             <li><i>2020</i> &nbsp;&nbsp;<Link to="https://youtu.be/w_4vIUz3dBA">Aladdin the Musical (Aladyn) – Million Miles Away "Milion Mil Stąd"</Link></li>
-            <li><i>2020</i> &nbsp;&nbsp;<Link to="https://youtu.be/-9_eUIc0QdY">Itazura na Kiss ED – Jikan yo Tomare</Link></li>
+            <li><i>2020</i> &nbsp;&nbsp;<Link to="https://youtu.be/-9_eUIc0QdY">Itazura na Kiss (ED) – Jikan yo Tomare</Link></li>
             <li><i>2019</i> &nbsp;&nbsp;<Link to="https://youtu.be/jDeo5SAnIUo">ニア Near</Link></li>
             <li><i>2019</i> &nbsp;&nbsp;<Link to="https://youtu.be/mTBVwwkPwkY">Porter Robinson - Shelter</Link></li>
             <li><i>2018</i> &nbsp;&nbsp;<Link to="https://youtu.be/im-NWec70kQ">Mulan - Lustro</Link></li>
-            <li><i>2018</i> &nbsp;&nbsp;<Link to="https://youtu.be/iuwtODXYukM">Gangsta ED - Yoru no Kuni</Link></li>
+            <li><i>2018</i> &nbsp;&nbsp;<Link to="https://youtu.be/iuwtODXYukM">Gangsta (ED) - Yoru no Kuni</Link></li>
             <li><i>2018</i> &nbsp;&nbsp;<Link to="https://youtu.be/briJEfUZw9w">Kopciuszek (2015) - Lavender’s Blue</Link></li>
             <li><i>2017</i> &nbsp;&nbsp;<Link to="https://youtu.be/IcRvqzKK_nE">NanoKarrin - BDTNP</Link></li>
             <li><i>2017</i> &nbsp;&nbsp;<Link to="https://youtu.be/bTJMPY4aMMc">Foilverb×Nakaji Motome - Fuurin to shinpaisei "Dzwonki"</Link></li>
@@ -24,7 +24,7 @@ const SecondPage = () => (
             <li><i>2014</i> &nbsp;&nbsp;<Link to="https://youtu.be/aDe88XUyfhs">Ao Haru Ride opening (OP) - Sekai wa Koi ni Ochiteiru</Link></li>
             <li><i>2014</i> &nbsp;&nbsp;<Link to="http://youtu.be/6rdvSgnUIWs">Orange Caramel & Nu'est - Dashing Through the Snow in High Heels</Link></li>
             <li><i>2014</i> &nbsp;&nbsp;<Link to="http://youtu.be/BAyaUSx83n8">Kanon - Last regrets "Ukłucie żalu"</Link></li>
-						<li><i>2013</i> &nbsp;&nbsp;<Link to="http://youtu.be/GPl-G6JayIs">Vampire Knight Guilty ED - Suna no Oshiro</Link></li>
+						<li><i>2013</i> &nbsp;&nbsp;<Link to="http://youtu.be/GPl-G6JayIs">Vampire Knight Guilty (ED) - Suna no Oshiro</Link></li>
             <li><i>2013</i> &nbsp;&nbsp;<Link to="http://youtu.be/FZCt3M_wW3k">Sword Art Online (OP 2) - Innocence</Link></li>
             <li><i>2013</i> &nbsp;&nbsp;<Link to="http://youtu.be/lvqlbWgOE3s">Btooom! (OP) - No Pain No Game</Link></li>
             <li><i>2013</i> &nbsp;&nbsp;<Link to="http://youtu.be/l6F5GWa4P-Y">Girls' Generation - Dear Mom</Link></li>
