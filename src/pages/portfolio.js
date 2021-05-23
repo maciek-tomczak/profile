06e7b1582c1e3cc12c10fd519ff51230ddba1938
maciek-tomczak <maciektomr@gmail.com>
2021-05-23 15:11:49 +0100

    fix brackets

--- src/pages/portfolio.js
+++ src/pages/portfolio.js
@@ -10,7 +10,7 @@ const SecondPage = () => (
     <p>Selected cover song mixing and mastering work at <Link to="http://nanokarrin.pl">NanoKarrin</Link>:</p>
     <ol class='fa-ul'>
             <li><i>2020</i> &nbsp;&nbsp;<Link to="https://youtu.be/HqDkCFoAn8M">DAOKO × Kenshi Yonezu - Uchiage Hanabi</Link></li>
-            <li><i>2020</i> &nbsp;&nbsp;<Link to="https://youtu.be/A6NMQ9UKlqI">Aldnoah.Zero ending (ED) 2 - aLIEz</Link></li>
+            <li><i>2020</i> &nbsp;&nbsp;<Link to="https://youtu.be/A6NMQ9UKlqI">Aldnoah.Zero ending (ED 2) - aLIEz</Link></li>
             <li><i>2020</i> &nbsp;&nbsp;<Link to="https://youtu.be/w_4vIUz3dBA">Aladdin the Musical (Aladyn) – Million Miles Away "Milion Mil Stąd"</Link></li>
             <li><i>2020</i> &nbsp;&nbsp;<Link to="https://youtu.be/-9_eUIc0QdY">Itazura na Kiss ED – Jikan yo Tomare</Link></li>
             <li><i>2019</i> &nbsp;&nbsp;<Link to="https://youtu.be/jDeo5SAnIUo">ニア Near</Link></li>
@@ -25,17 +25,17 @@ const SecondPage = () => (
             <li><i>2014</i> &nbsp;&nbsp;<Link to="http://youtu.be/6rdvSgnUIWs">Orange Caramel & Nu'est - Dashing Through the Snow in High Heels</Link></li>
             <li><i>2014</i> &nbsp;&nbsp;<Link to="http://youtu.be/BAyaUSx83n8">Kanon - Last regrets "Ukłucie żalu"</Link></li>
 						<li><i>2013</i> &nbsp;&nbsp;<Link to="http://youtu.be/GPl-G6JayIs">Vampire Knight Guilty ED - Suna no Oshiro</Link></li>
-            <li><i>2013</i> &nbsp;&nbsp;<Link to="http://youtu.be/FZCt3M_wW3k">Sword Art Online OP 2- Innocence</Link></li>
-            <li><i>2013</i> &nbsp;&nbsp;<Link to="http://youtu.be/lvqlbWgOE3s">Btooom! OP - No Pain No Game</Link></li>
+            <li><i>2013</i> &nbsp;&nbsp;<Link to="http://youtu.be/FZCt3M_wW3k">Sword Art Online (OP 2) - Innocence</Link></li>
+            <li><i>2013</i> &nbsp;&nbsp;<Link to="http://youtu.be/lvqlbWgOE3s">Btooom! (OP) - No Pain No Game</Link></li>
             <li><i>2013</i> &nbsp;&nbsp;<Link to="http://youtu.be/l6F5GWa4P-Y">Girls' Generation - Dear Mom</Link></li>
             <li><i>2013</i> &nbsp;&nbsp;<Link to="http://youtu.be/QcEYTeGfmD8">Final Fantasy XIII-2 - Yeul Theme</Link></li>
-            <li><i>2013</i> &nbsp;&nbsp;<Link to="http://youtu.be/N8eoDEZjTYo">Kuroshitsuji ED - I'm alive</Link></li>
+            <li><i>2013</i> &nbsp;&nbsp;<Link to="http://youtu.be/N8eoDEZjTYo">Kuroshitsuji (ED) - I'm alive</Link></li>
             <li><i>2013</i> &nbsp;&nbsp;<Link to="https://youtu.be/Tu7u9kTNC2A">Final Fantasy XIII-2 - Noel Theme "The Last Travel"</Link></li>
-            <li><i>2012</i> &nbsp;&nbsp;<Link to="http://youtu.be/YvMToqVmPKY">Sword Art Online OP 1 - Crossing Field</Link></li>
-            <li><i>2012</i> &nbsp;&nbsp;<Link to="http://youtu.be/fX4Cju9yV5Q">School Days OP - Innocent Blue</Link></li>
-            <li><i>2012</i> &nbsp;&nbsp;<Link to="https://youtu.be/YvgfhN5VLMQ">Higurashi no Naku Koro Ni OP</Link></li>
-            <li><i>2012</i> &nbsp;&nbsp;<Link to="https://youtu.be/YvMToqVmPKY">Sword Art Online OP - Crossing Field</Link></li>
-            <li><i>2012</i> &nbsp;&nbsp;<Link to="https://youtu.be/MfRg7WjP4IQ">Bleach OP 12 - Change</Link></li>
+            <li><i>2012</i> &nbsp;&nbsp;<Link to="http://youtu.be/YvMToqVmPKY">Sword Art Online (OP 1) - Crossing Field</Link></li>
+            <li><i>2012</i> &nbsp;&nbsp;<Link to="http://youtu.be/fX4Cju9yV5Q">School Days (OP) - Innocent Blue</Link></li>
+            <li><i>2012</i> &nbsp;&nbsp;<Link to="https://youtu.be/YvgfhN5VLMQ">Higurashi no Naku Koro Ni (OP)</Link></li>
+            <li><i>2012</i> &nbsp;&nbsp;<Link to="https://youtu.be/YvMToqVmPKY">Sword Art Online (OP) - Crossing Field</Link></li>
+            <li><i>2012</i> &nbsp;&nbsp;<Link to="https://youtu.be/MfRg7WjP4IQ">Bleach (OP 12) - Change</Link></li>
     </ol>
     <p>Selected mastering work and assistance on projects:</p>
     <ol class='fa-ul'>
